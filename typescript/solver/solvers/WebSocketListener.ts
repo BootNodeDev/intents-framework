b470b0aa8efee05631254d3479308da57a536f23
fernandomg <fernando.greco@gmail.com>
2025-03-11 19:39:00 +0100

    feat: websocket listener

---
+++ typescript/solver/solvers/WebSocketListener.ts
@@ -0,0 +1,206 @@
+import type { Logger } from "../logger.js";
+import type { ParsedArgs } from "./BaseFiller.js";
+
+import { WebSocket } from "ws";
+
+export interface ConnectionUpdate {
+  type: "connected";
+  data: {
+    clientCount: number;
+  };
+  timestamp: string;
+}
+
+export type WebSocketMessage = ConnectionUpdate;
+
+export abstract class WebSocketListener<
+  TParsedArgs extends ParsedArgs,
+> {
+  private ws: WebSocket | null = null;
+  private reconnectAttempts = 0;
+  private maxReconnectAttempts = 5;
+  private reconnectDelay = 1_000;
+  private pingInterval: NodeJS.Timeout | null = null;
+  private pongTimeout: NodeJS.Timeout | null = null;
+  private lastPingTime = 0;
+  private wsUrl: string;
+
+  protected onOpen() {}
+  protected onClose() {}
+  protected onError() {}
+
+  protected constructor(
+    private readonly metadata: {
+      webSocket: {
+        url: string;
+        clientOptions: WebSocket.ClientOptions;
+        options?: {
+          maxReconnectAttempts?: number;
+          reconnectDelay?: number;
+        };
+      };
+      protocolName: string;
+    },
+    private readonly log: Logger,
+  ) {
+    this.maxReconnectAttempts =
+      this.metadata.webSocket.options?.maxReconnectAttempts ||
+      this.maxReconnectAttempts;
+    this.reconnectDelay =
+      this.metadata.webSocket.options?.reconnectDelay || this.reconnectDelay;
+
+    this.wsUrl = this.metadata.webSocket.url;
+  }
+
+  private connect(): void {
+    try {
+      this.ws = new WebSocket(
+        this.wsUrl,
+        this.metadata.webSocket.clientOptions,
+      );
+      this.setupEventListeners();
+    } catch (error) {
+      this.log.error({
+        msg: "Failed to create WebSocket connection",
+        error: JSON.stringify(error),
+      });
+      this.handleReconnect();
+    }
+  }
+
+  private setupEventListeners(): void {
+    if (!this.ws) return;
+
+    this.ws.on("open", (): void => {
+      this.log.info({ msg: "WebSocket connection established" });
+      this.reconnectAttempts = 0;
+      this.reconnectDelay = 1000;
+      this.setupPingInterval();
+      this.onOpen?.();
+    });
+
+    this.ws.on("close", (): void => {
+      this.log.info({ msg: "WebSocket connection closed" });
+      this.cleanupPingInterval();
+      this.onClose?.();
+      this.handleReconnect();
+    });
+
+    this.ws.on("error", (error: Error): void => {
+      this.log.error({
+        msg: "WebSocket error occurred",
+        error: JSON.stringify(error),
+      });
+      this.cleanupPingInterval();
+    });
+
+    this.ws.on("message", (data: Buffer): void => {
+      try {
+        const message = JSON.parse(data.toString());
+        this.handleMessage(message);
+      } catch (error) {
+        this.log.error("Error parsing message:", error);
+      }
+    });
+
+    this.ws.on("pong", () => {
+      if (this.pongTimeout) {
+        clearTimeout(this.pongTimeout);
+        this.pongTimeout = null;
+      }
+
+      const latency = Date.now() - this.lastPingTime;
+      this.log.debug({ msg: "WebSocket pong", latency });
+    });
+  }
+
+  private handleReconnect(): void {
+    this.cleanupPingInterval();
+    if (this.reconnectAttempts < this.maxReconnectAttempts) {
+      this.log.info({
+        msg: `Attempting to reconnect... (${this.reconnectAttempts + 1}/${this.maxReconnectAttempts})`,
+      });
+
+      setTimeout(() => {
+        this.reconnectAttempts++;
+        this.reconnectDelay *= 2; // Exponential backoff
+        this.connect();
+      }, this.reconnectDelay);
+    } else {
+      this.log.error({ msg: "Max reconnection attempts reached" });
+    }
+  }
+
+  private setupPingInterval(): void {
+    // Send a ping every 15 seconds
+    this.pingInterval = setInterval(() => {
+      if (this.ws && this.ws.readyState === WebSocket.OPEN) {
+        this.lastPingTime = Date.now();
+
+        this.ws.ping();
+
+        // Set a timeout to close and reconnect if we don't receive a pong within 5 seconds
+        this.pongTimeout = setTimeout(() => {
+          this.log.warn({
+            msg: "No pong received within timeout, closing connection...",
+          });
+          if (this.ws) {
+            this.ws.close();
+          }
+        }, 5_000);
+      }
+    }, 15_000);
+  }
+
+  private cleanupPingInterval(): void {
+    if (this.pingInterval) {
+      clearInterval(this.pingInterval);
+      this.pingInterval = null;
+    }
+    if (this.pongTimeout) {
+      clearTimeout(this.pongTimeout);
+      this.pongTimeout = null;
+    }
+  }
+
+  create() {
+    return async (
+      handler: (
+        args: TParsedArgs,
+        originChainName: string,
+        blockNumber: number,
+      ) => void,
+    ) => {
+      this.connect();
+
+      if (!this.ws) {
+        this.log.debug({ msg: "WebSocket connection not yet established" });
+        return;
+      }
+
+      this.ws.on("message", (data: Buffer): void => {
+        try {
+          const args = this.parseEventArgs(data);
+          handler(args, '', -1);
+        } catch (error) {
+          this.log.error("Error parsing message:", error);
+        }
+      });
+    };
+  }
+
+  private handleMessage(data: WebSocketMessage): void {
+    switch (data.type) {
+      // case "connected":
+      //   console.debug("Connected to the server");
+      //   console.debug(`Client count: ${data.data.clientCount}`);
+      //   console.debug(`Timestamp: ${data.timestamp}`);
+      //   break;
+      default:
+        console.error("Unknown message type:", data);
+    }
+  }
+  protected abstract parseEventArgs(
+    args: Buffer,
+  ): TParsedArgs;
+}
